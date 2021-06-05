8cdb6125c391ff15c67e2102aef07435842fac6d
Fastus001 <tkarczmarzyk@gmail.com>
2021-06-05 10:04:10 +0200

    first commit

---
+++ src/main/java/pl/fastus/matmaster/MatMasterApplication.java
@@ -0,0 +1,13 @@
+package pl.fastus.matmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/fastus/matmaster/MatMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fastus.matmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
