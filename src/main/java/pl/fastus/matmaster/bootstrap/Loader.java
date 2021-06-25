7b32b81ed2cd3e6abaa70b17bb0c7cb87400c4c2
Fastus <fastus6666@gamil.com>
2021-06-25 15:56:56 +0200

    little modifications in loader

--- src/main/java/pl/fastus/matmaster/bootstrap/Loader.java
+++ src/main/java/pl/fastus/matmaster/bootstrap/Loader.java
@@ -19,6 +19,7 @@ import java.util.List;
 @Component
 public class Loader implements CommandLineRunner {
 
+    private static final Long ID_PHOTO = 7L;
     private final BlogPostService blogPostService;
     private final ShopItemService shopItemService;
 
@@ -45,18 +46,21 @@ public class Loader implements CommandLineRunner {
     }
 
     private void loadBlogPosts() {
-        Paragraph paragraph1 = Paragraph.builder().headerText("Header1").text("Text1").imageId(6L).build();
+        Paragraph paragraph1 = Paragraph.builder().headerText("Header1").text("Ut in nunc nec elit aliquet dignissim. Vivamus eu congue ligula. Praesent sed semper metus, vel varius tortor. Duis id tincidunt elit. Maecenas nisl massa, aliquam id nulla at, ornare euismod libero. Nunc volutpat ut odio ac vulputate. Sed dictum nisl ac ipsum porttitor pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel leo diam. Etiam non sollicitudin dui, quis maximus nunc. Proin lectus leo, tristique at neque nec, pellentesque cursus lacus. Fusce ultricies orci vitae felis eleifend finibus nec a turpis. Mauris gravida egestas vulputate.").imageId(ID_PHOTO).build();
 
-        Paragraph paragraph2 = Paragraph.builder().headerText("Header2").text("Text2").imageId(6L).build();
+        Paragraph paragraph2 = Paragraph.builder().headerText("Header2").text("Text2").imageId(ID_PHOTO).build();
 
-        Paragraph paragraph3 = Paragraph.builder().headerText("Header3").text("Text3").build();
+        Paragraph paragraph3 = Paragraph.builder().headerText("Header3").text("Ut in nunc nec elit aliquet dignissim. Vivamus eu congue ligula. Praesent sed semper metus, vel varius tortor. Duis id tincidunt elit. Maecenas nisl massa, aliquam id nulla at, ornare euismod libero. Nunc volutpat ut odio ac vulputate. Sed dictum nisl ac ipsum porttitor pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel leo diam. Etiam non sollicitudin dui, quis maximus nunc. Proin lectus leo, tristique at neque nec, pellentesque cursus lacus. Fusce ultricies orci vitae felis eleifend finibus nec a turpis. Mauris gravida egestas vulputate.").build();
+
+        Paragraph paragraph4 = Paragraph.builder().headerText("Header2").text("Aliquam efficitur erat pulvinar lorem ultricies sollicitudin. In magna turpis, pharetra eu rhoncus et, dignissim vel nisl. Pellentesque sapien nunc, pulvinar eget molestie ut, faucibus eget mi. Donec auctor magna velit, eu scelerisque quam lobortis a. Nunc commodo dolor eu pharetra finibus. Aliquam erat volutpat. Nulla dapibus ligula ut tempor vestibulum. Suspendisse luctus imperdiet turpis, vehicula dapibus tortor sollicitudin non. Nulla facilisi. Aliquam erat volutpat. Etiam nec tellus lectus.").imageId(ID_PHOTO).build();
 
         BlogPostRequest blogPost = new BlogPostRequest().setTitle("Post 1")
                 .setParagraphs(List.of(paragraph1, paragraph2, paragraph3))
-                .setHeaderImageId(6L);
+                .setHeaderImageId(ID_PHOTO);
 
         BlogPostRequest blogPost1 = new BlogPostRequest().setTitle("Post 2")
-                .setHeaderImageId(6L);
+                .setParagraphs(List.of(paragraph4))
+                .setHeaderImageId(ID_PHOTO);
 
 
         blogPostService.createBlogPost(blogPost);
